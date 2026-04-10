d010fc8a3ef91f94b055f090bf1eec517d4be4a5
tunglx2 <tunglx2@vng.com.vn>
2026-04-10 15:51:24 +0700

    fix wrong method

--- src/apis/chat.api.js
+++ src/apis/chat.api.js
@@ -2,7 +2,7 @@ const { verifyToken } = require('../middleware/authorize');
 const { ChatFlowManager } = require('../modules/chat-bot/chat-flow-manager');
 
 function registerChatAPIs(app) {
-	app.get('/chat/message', verifyToken, (req, res) => {
+	app.post('/chat/message', verifyToken, (req, res) => {
 		ChatFlowManager.processAction(req.body);
 		res.status(200).send({ error_code: code, data: null, error_message: null });
 	});
